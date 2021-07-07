ee37127397801f66a891f2a9b8cc730888412211
mrkononenko <mkononenko68@gmail.com>
2021-07-07 10:16:09 +0300

    file Code for task0 added

--- Код до Task 0 .py
+++ Код до Task 0 .py
@@ -1,6 +1,3 @@
-# My_first_repository
-Studying
-![image](https://user-images.githubusercontent.com/86930406/124711393-b25a6b00-df06-11eb-991e-bd219790aeb9.png)
 import random
 random.seed()
 
@@ -23,4 +20,3 @@ for i in range(len(list1)):
         list2.append(list1[i])
 list2.sort(reverse=True)
 print("List with odd numbers in descending order: \n", list2)
-
